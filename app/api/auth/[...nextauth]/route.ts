0c682d356e90335111a895a8e441100b6f0ab1a8
usman1058 <techinfo1058@gmail.com>
2025-08-12 19:46:19 +0500

    14

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
